925b8fe8ddbae07a4f38560929152ae1bdb097d5
Aditya Singh <99306842+3IceCube@users.noreply.github.com>
2022-07-20 20:26:24 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,90 @@
+function copy1() {
+  document.getElementById("link").value = "http://niggy.duckdns.org/Hindi_2022/Anek.mkv";
+  document.getElementById("link").select();
+  document.execCommand("copy");
+  alert("Link copied to clipboard,");
+}
+
+function copy2() {
+  document.getElementById("link").value = "http://niggy.duckdns.org/Hindi_2022/Attack.mkv";
+  document.getElementById("link").select();
+  document.execCommand("copy");
+  alert("Link copied to clipboard,");
+}
+
+function copy3() {
+  document.getElementById("link").value = "http://niggy.duckdns.org/Hindi_2022/Forensic.mp4";
+  document.getElementById("link").select();
+  document.execCommand("copy");
+  alert("Link copied to clipboard,");
+}
+
+function copy4() {
+  document.getElementById("link").value = "http://niggy.duckdns.org/Hindi_2022/Hurdang.mkv";
+  document.getElementById("link").select();
+  document.execCommand("copy");
+  alert("Link copied to clipboard,");
+}
+
+function copy5() {
+  document.getElementById("link").value = "http://niggy.duckdns.org/Hindi_2022/Jaadugar.mkv";
+  document.getElementById("link").select();
+  document.execCommand("copy");
+  alert("Link copied to clipboard,");
+}
+
+function copy6() {
+  document.getElementById("link").value = "http://niggy.duckdns.org/Hindi_2022/Janhit_Mein_Jaari.mkv";
+  document.getElementById("link").select();
+  document.execCommand("copy");
+  alert("Link copied to clipboard,");
+}
+
+function copy7() {
+  document.getElementById("link").value = "http://niggy.duckdns.org/Hindi_2022/Jersey.mkv";
+  document.getElementById("link").select();
+  document.execCommand("copy");
+  alert("Link copied to clipboard,");
+}
+
+function copy8() {
+  document.getElementById("link").value = "http://niggy.duckdns.org/Hindi_2022/Mere_Desh_Ki_Dharti.mkv";
+  document.getElementById("link").select();
+  document.execCommand("copy");
+  alert("Link copied to clipboard,");
+}
+
+function copy9() {
+  document.getElementById("link").value = "http://niggy.duckdns.org/Hindi_2022/Operation_Romeo.mkv";
+  document.getElementById("link").select();
+  document.execCommand("copy");
+  alert("Link copied to clipboard,");
+}
+
+function copy10() {
+  document.getElementById("link").value = "http://niggy.duckdns.org/Hindi_2022/Runway_34.mkv";
+  document.getElementById("link").select();
+  document.execCommand("copy");
+  alert("Link copied to clipboard,");
+}
+
+function copy11() {
+  document.getElementById("link").value = "http://niggy.duckdns.org/Hindi_2022/Samrat_Prithviraj.mkv";
+  document.getElementById("link").select();
+  document.execCommand("copy");
+  alert("Link copied to clipboard,");
+}
+
+function copy12() {
+  document.getElementById("link").value = "http://niggy.duckdns.org/Hindi_2022/Shabaash_Mithu.mkv";
+  document.getElementById("link").select();
+  document.execCommand("copy");
+  alert("Link copied to clipboard,");
+}
+
+function copy13() {
+  document.getElementById("link").value = "http://niggy.duckdns.org/Hindi_2022/Zombivli.mkv";
+  document.getElementById("link").select();
+  document.execCommand("copy");
+  alert("Link copied to clipboard,");
+}
